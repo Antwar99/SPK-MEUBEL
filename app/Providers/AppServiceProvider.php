20070084aeb8c71d489e9aa08e0e7e9f48043232
SYAIFUL ANWAR <ipoelsakily@gamil.com>
2025-07-21 15:16:23 +0700

    appserviceprovider fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +28,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Paksa penggunaan HTTPS jika bukan di local
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
