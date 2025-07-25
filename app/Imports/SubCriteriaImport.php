e538ebb2c52fe8c7acfada3959dcb3147ae13989
Antwar99 <133996425+Antwar99@users.noreply.github.com>
2025-07-25 16:31:25 +0700

    Update SubCriteriaImport.php

--- app/Imports/SubCriteriaImport.php
+++ app/Imports/SubCriteriaImport.php
@@ -7,15 +7,18 @@ use Maatwebsite\Excel\Concerns\ToModel;
 
 class SubCriteriaImport implements ToModel
 {
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
     public function model(array $row)
     {
+        // Skip the header row
+        if ($row[0] === 'Kriteria Kode' || $row[0] == null) {
+            return null;
+        }
+
         return new SubCriteria([
-            //
+            'criteria_kode' => $row[0],
+            'name'          => $row[1],
+            'level'         => $row[2],
+            'value'         => $row[3],
         ]);
     }
 }
